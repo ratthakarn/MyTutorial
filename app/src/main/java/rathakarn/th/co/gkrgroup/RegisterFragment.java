e45434472e758788df9fdc162a13dd836a531dc5
rathakarn <gkrgroup.co.th@gmail.com>
2019-01-30 09:25:56 +0700

    Create Menu finish

--- app/src/main/java/rathakarn/th/co/gkrgroup/RegisterFragment.java
+++ app/src/main/java/rathakarn/th/co/gkrgroup/RegisterFragment.java
@@ -6,6 +6,9 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -26,18 +29,50 @@ public class RegisterFragment extends Fragment {
 
 //        Create Toolbar
 
+        createToolbar();
+    } //Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemUpload) {
+            checkValue();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void checkValue() {
+
+//        Get Value From Edittext
+
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_register,menu);
+    }
+
+    private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
         ((MainActivity)getActivity()).setSupportActionBar(toolbar);
-((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
-((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
-((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-       getActivity().getSupportFragmentManager().popBackStack();
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+        setHasOptionsMenu(true);
+
+
+
     }
-});
-    } //Main Method
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
